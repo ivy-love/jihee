4c4f8430c761e4c5412ed87d78725ca8bb38d724
Jihee <62944687+ivy-love@users.noreply.github.com>
2022-10-18 01:44:23 +0900

    style : 업데이트

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,12 +3,59 @@ $(document).ready(function(){
     //AOS 라이브러리 초기화
     AOS.init();
 
-    function Aos_Mobile() {
-        if(matchMedia("(max-width: 786px) and (min-width: 320px)").matches) {
-            // $(".portfolio-txt").attr('data-aos', 'slide-left');
-        }
+
+
+    // $(window).scroll(function() {
+    //     if($(this).scrollTop() == 0)
+    //         $('header').css('background','rgba(255, 255, 255)');
+    //     else
+    //         $('header').css({background:"rgba(255, 255, 255, 0.6)"});
+    // });
+
+
+
+
+
+    $(".menu-toggle").mouseenter(function(){
+        $(this).addClass("on")
+    });
+    $(".menu-toggle").mouseleave(function(){
+        $(this).removeClass("on")
+    })
+    
+
+
+
+
+
+    function aosMobile() {
+
+        const isMobile = window.innerWidth < 1200;
+        console.log(isMobile);
+        if(!isMobile) return
+        $(".portfolio-txt").addClass("on");
+
+        $(window).scroll(function () {
+            const top = window.scrollY || window.pageYOffset;
+            console.log(top);
+          });
+       
+
+
+        // if(matchMedia("(max-width: 786px) and (min-width: 320px)").matches) {
+            
+        //     $(".portfolio-txt").attr({
+        //         'data-aos':'slide-left',
+        //         'data-aos-delay':'3000'
+        //     });
+
+        //     $(".even .portfolio-txt").attr({
+        //         'data-aos':'slide-right',
+        //         'data-aos-delay':'1000'
+        //     });
+        // }
     };
-    Aos_Mobile();
+    aosMobile();
 
 
 
