e17b5dd4778e63cd5759b4a28a262024f7c2d073
Jihee <62944687+ivy-love@users.noreply.github.com>
2022-10-18 15:35:04 +0900

    fix : 햄버거버튼 버그 수정

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,14 +15,40 @@ $(document).ready(function(){
 
 
 
-
+    //menu-toggle 마우스 올렸을 때 width 값 변경
     $(".menu-toggle").mouseenter(function(){
-        $(this).addClass("on")
+        $(this).addClass("on");
     });
     $(".menu-toggle").mouseleave(function(){
-        $(this).removeClass("on")
+        $(this).removeClass("on");
+    });
+
+    //햄버거 버튼 열고 닫기
+    $(".menu-toggle").click(function(){
+        $(this).siblings(".menu-bg").css('left','0');
+    });
+    $(".menu-close").click(function(){
+        $(this).parent(".menu-bg").css('left','-100%');
+    });
+
+    // menu-item 클릭 시 menu-bg 닫히고,
+    //클릭한 페이지로 스코롤 이동
+    $(".link_profile").click(function(){
+        $(".menu-bg").css('left','-100%');
+        const offset = $('.intro').offset();
+          $('html').animate({scrollTop : offset.top}, 400);
     })
-    
+    $(".link_portfolio").click(function(){
+        $(".menu-bg").css('left','-100%');
+        const offset = $('.portfolio').offset();
+          $('html').animate({scrollTop : offset.top - 98}, 400);
+    })
+    $(".link_contact").click(function(){
+        $(".menu-bg").css('left','-100%');
+        const offset = $('.contact').offset();
+          $('html').animate({scrollTop : offset.top - 98}, 400);
+    })
+
 
 
 
@@ -56,7 +82,4 @@ $(document).ready(function(){
         // }
     };
     aosMobile();
-
-
-
 });
