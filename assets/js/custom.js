7450b3a7cbe3074b33f3f43b97c024d78025b13d
Jihee <62944687+ivy-love@users.noreply.github.com>
2022-12-01 02:53:05 +0900

    refactor : 코드 리팩토링

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -19,21 +19,41 @@ $(document).ready(function(){
         $(this).parent(".menu-bg").css('left','-100%');
     });
 
+
+    //.contact .email-txt 스크롤 했을 때 스케일 확대
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.from(".email-txt", {
+        scrollTrigger: {
+          pin: true,
+          trigger: ".contact",
+          scrub: true,
+        },
+        start: "top top",
+        scale: 0.2,
+        autoAlpha: 0,
+        ease: "sine.out",
+    });
+
+    // .email-txt hover했을 때 .email-txt-sub .active 추가
+    $(".email-txt").hover(function(){
+        $(".email-txt-sub").toggleClass("active")
+    })
+
     // menu-item 클릭 시 menu-bg 닫히고,
     //클릭한 페이지로 스코롤 이동
     $(".link_profile").click(function(){
         $(".menu-bg").css('left','-100%');
-        const offset = $('.intro').offset();
+        let offset = $('.intro').offset();
           $('html').animate({scrollTop : offset.top}, 400);
     })
     $(".link_portfolio").click(function(){
         $(".menu-bg").css('left','-100%');
-        const offset = $('.portfolio').offset();
+        let offset = $('.portfolio').offset();
           $('html').animate({scrollTop : offset.top - 98}, 400);
     })
     $(".link_contact").click(function(){
         $(".menu-bg").css('left','-100%');
-        const offset = $('.contact').offset();
+        let offset = $('.contact').offset();
           $('html').animate({scrollTop : offset.top - 98}, 400);
     })
 
